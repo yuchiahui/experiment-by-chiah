2575f931a8bf412fdc27a6f14d88ed44cdf9ea9c
高科技黑手 <45622996+pclin76@users.noreply.github.com>
2019-01-09 13:25:47 +0800

    Update schema.sql

--- schemas.sql
+++ schemas.sql
@@ -1,26 +1,13 @@
--- Database: "Chiah"
-
--- DROP DATABASE "Chiah";
-
-CREATE DATABASE "Chiah"
-  WITH OWNER = "Chiah"
-       ENCODING = 'UTF8'
-       TABLESPACE = pg_default
-       CONNECTION LIMIT = -1;
-
----------------------------------------------------------- 
 CREATE TABLE "public"."teacher" (
 	"id" smallserial PRIMARY KEY,
 	"name" varchar NOT NULL
 );
-	
-----------------------------------
+
 CREATE TABLE "public"."student" (
 	"id" serial PRIMARY KEY,
 	"name" varchar NOT NULL
 );
 
-----------------------------------
 CREATE TABLE "public"."clazz" (
 	"id" bigserial PRIMARY KEY,
 	"teacher" smallint NOT NULL REFERENCES "teacher"("id") ON UPDATE CASCADE ON DELETE RESTRICT,
