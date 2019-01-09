eb0e4514f1814748b6ae6760af874a1da077c055
P-C Lin <p@w3c.org.tw>
2019-01-09 13:15:01 +0800

    新增、修改測試相關設定

--- src/test/java/com/bigbigmall/experiment/ExperimentApplicationTests.java
+++ src/test/java/com/bigbigmall/experiment/ExperimentApplicationTests.java
@@ -1,7 +1,10 @@
 package com.bigbigmall.experiment;
 
+import javax.sql.DataSource;
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,7 +12,18 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ExperimentApplicationTests {
 
+	@Autowired
+	private DataSource dataSource;
+
 	@Test
 	public void contextLoads() {
+	}
+
+	/**
+	 * 參考自 https://www.baeldung.com/spring-boot-tomcat-connection-pool
+	 */
+	@Test
+	public void givenTomcatConnectionPoolInstanceWhenCheckedPoolClassNameThenCorrect() {
+		assertThat(dataSource.getClass().getName()).isEqualTo("org.apache.tomcat.jdbc.pool.DataSource");
 	}
 }
