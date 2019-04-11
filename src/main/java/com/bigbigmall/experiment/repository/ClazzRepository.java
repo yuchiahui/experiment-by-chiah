8ea8f1e38c9996abd544dbc4faa65a6ec5f29f21
yuchiahui <yuchiahui1992@gmail.com>
2019-04-11 09:37:15 +0800

    commit

--- src/main/java/com/bigbigmall/experiment/repository/ClazzRepository.java
+++ src/main/java/com/bigbigmall/experiment/repository/ClazzRepository.java
@@ -1,6 +1,7 @@
 package com.bigbigmall.experiment.repository;
 
 import com.bigbigmall.experiment.entity.Clazz;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +12,6 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface ClazzRepository extends JpaRepository<Clazz, Long> {
+	
+//	Object o = new Pageable();
 }
